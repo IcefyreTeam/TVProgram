e2520cd9877fe65aca72a6860c4a004451c760ee
skolev <svetliokolev@gmail.com>
2013-09-01 23:28:12 +0300

    Updated home page
    Added list view binding in home page. Created snapped mode view

---
+++ BulgarianTvGuide/BulgarianTvGuide/pages/home/homecodebehind.js
@@ -0,0 +1,39 @@
+﻿(function () {
+
+    //var goToComputerDetailsPage = function (invokeEvent) {
+    //    WinJS.Navigation.navigate("/pages/computerdetails/computerdetails.html", {
+    //        indexInComputersList: invokeEvent.detail.itemIndex
+    //    });
+    //}
+
+    //WinJS.Utilities.markSupportedForProcessing(goToComputerDetailsPage);
+
+    WinJS.Namespace.define("HomeCodeBehind", {
+        callLoadPrograms: function () {
+            //ViewModels.loadPrograms();
+        },
+    //WinJS.Namespace.define("HomeCodeBehind", {
+    //    callLoadPrograms: function () {
+    //        new WinJS.Promise(function (com) {
+    //            Data.initPrograms();
+    //            com();
+    //        }).then(function () {
+    //            ViewModels.loadPrograms();
+    //        });
+    //        var x = 389543;
+    //    },
+
+    //    callInitPrograms: function () {
+    //        new WinJS.Promise(function (com) {
+    //            Data.initPrograms();
+    //            com();
+    //        }).then(function () {
+    //            ViewModels.loadPrograms();
+    //        }).then(function myfunction() {
+    //            var w = 389543;
+
+    //        });
+    //    }
+        //goToComputerDetailsPage: goToComputerDetailsPage
+    })
+})();
