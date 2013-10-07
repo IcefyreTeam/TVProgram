bc148a1fb553c10b3b72f4c360521c4450e92998
skolev <svetliokolev@gmail.com>
2013-10-07 12:32:06 +0300

    last folder with solution added

---
+++ last/BulgarianTvGuide/js/defaultcodebehind.js
@@ -0,0 +1,12 @@
+﻿(function () {
+
+    var goToAddComputer = function(){
+        WinJS.Navigation.navigate("/pages/addcomputer/addcomputer.html");
+    }
+
+    WinJS.Utilities.markSupportedForProcessing(goToAddComputer);
+
+    WinJS.Namespace.define("DefaultCodeBehind", {
+        goToAddComputer: goToAddComputer
+        });
+})()
